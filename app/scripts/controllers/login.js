8d3a4096af73f761915b240c3cb207a16f32c978
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-05 19:45:38 -0500

    Added login route.

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whatsCookingApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the whatsCookingApp
+ */
+angular.module('whatsCookingApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
