edbe1d6e9049a42fbdb7bb0d8d80bd7ec0961f76
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-08 12:41:54 -0500

    Created authentication service.

---
+++ app/scripts/services/authenticationservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name whatsCookingApp.AuthenticationService
+ * @description
+ * # AuthenticationService
+ * Service in the whatsCookingApp.
+ */
+angular.module('whatsCookingApp')
+  .service('AuthenticationService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
