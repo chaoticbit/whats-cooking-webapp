715abc190dfaefde9582fb071718977f6a06e1f3
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-11 13:30:10 -0400

    Added api key constants.

--- app/scripts/constants/apiconfig.js
+++ app/scripts/constants/apiconfig.js
@@ -2,8 +2,9 @@
 
 angular.module('whatsCookingApp').factory('ApiConfig', function() {
     var API_URL = 'http://localhost/whats-cooking-api/api';
-
+    var API_KEY = 'food-food';    
     return {
-        API_URL: API_URL
+        API_URL: API_URL,
+        API_KEY: API_KEY,
     };
 });
