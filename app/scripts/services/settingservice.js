043e695690742389dce7d4b7a33c808f464fbe1b
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-11 13:36:13 -0400

    Added user profile related services.

--- app/scripts/services/settingservice.js
+++ app/scripts/services/settingservice.js
@@ -7,6 +7,22 @@
  * # SettingService
  * Service in the whatsCookingApp.
  */
-angular.module('whatsCookingApp').service('SettingService', function () {
-    
+angular.module('whatsCookingApp').service('SettingService', function ($rootScope, $http, ApiConfig) {
+    var token = $rootScope.userProfile.token;
+
+    this.getUserProfile = function() {
+        return $http({
+            method: 'GET',
+            url: ApiConfig.API_URL + '/Settings/userprofile',            
+            headers: {'x-api-key': btoa(ApiConfig.API_KEY),'token': token}	
+        });
+    };
+    this.saveUserProfile = function(payload) {
+		return $http({
+			method: 'POST',
+			url: ApiConfig.API_URL + '/Settings/userprofile',
+            data: payload,		
+            headers: {'x-api-key': btoa(ApiConfig.API_KEY),'token': token}	
+		});	  
+	};
 });
