b8680fbb3f6ac9e9f2d103e2639b1f69f0b2278b
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-23 19:00:50 -0400

    Added profile and settings route.

---
+++ app/scripts/controllers/settings.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whatsCookingApp.controller:SettingsCtrl
+ * @description
+ * # SettingsCtrl
+ * Controller of the whatsCookingApp
+ */
+angular.module('whatsCookingApp')
+  .controller('SettingsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whatsCookingApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the whatsCookingApp
+ */
+angular.module('whatsCookingApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
