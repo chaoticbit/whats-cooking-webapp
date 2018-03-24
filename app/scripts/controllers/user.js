986f4ee8787c05e89777e96b2d3321ba348bdb73
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-23 20:32:31 -0400

    Done profile basic design.

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -7,11 +7,21 @@
  * # UserCtrl
  * Controller of the whatsCookingApp
  */
-angular.module('whatsCookingApp')
-  .controller('UserCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('whatsCookingApp').controller('UserCtrl', function ($rootScope, $routeParams, $scope, $window) {    
+    $scope.username = $routeParams.username;
+
+    if(screen.width > 480) {
+        $(window).on('scroll',function() {
+            var distanceScrolled = $(this).scrollTop();
+            $('.bg-img').css('-webkit-filter', 'blur('+distanceScrolled/20+'px)');
+        });
+        
+        $('.top-recipes > ul > li').hover(function(){
+           $(this).find('.star').removeClass('transDown').addClass('transUp');
+           $(this).find('.upvotecnt').removeClass('transDown').addClass('transUp');
+        },function(){
+           $(this).find('.upvotecnt').removeClass('transUp').addClass('transDown');
+           $(this).find('.star').removeClass('transUp').addClass('transDown');
+        });
+    }
+});
