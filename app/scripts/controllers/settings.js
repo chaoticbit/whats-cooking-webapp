67d747cee03a49d8260978398579bbf13ecdf5d4
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-24 11:10:28 -0400

    Added new settings scss file,

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -7,11 +7,6 @@
  * # SettingsCtrl
  * Controller of the whatsCookingApp
  */
-angular.module('whatsCookingApp')
-  .controller('SettingsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('whatsCookingApp').controller('SettingsCtrl', function ($rootScope, $scope, $window, SettingService) {    
+
+});
