1351a0556f7be86770acca25b59ec03b7c69f007
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-24 09:20:51 -0400

    Completed profile page info display.

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -7,21 +7,35 @@
  * # UserCtrl
  * Controller of the whatsCookingApp
  */
-angular.module('whatsCookingApp').controller('UserCtrl', function ($rootScope, $routeParams, $scope, $window) {    
+angular.module('whatsCookingApp').controller('UserCtrl', function ($rootScope, $routeParams, $scope, $window, UserService) {    
     $scope.username = $routeParams.username;
+    $scope.userprofile = {};
+    $scope.titles = ['gender','email','city','state','country','preferred cuisine','food group','spiciness','calorie intake'];
 
-    if(screen.width > 480) {
+    if(screen.width > 480) {        
         $(window).on('scroll',function() {
             var distanceScrolled = $(this).scrollTop();
             $('.bg-img').css('-webkit-filter', 'blur('+distanceScrolled/20+'px)');
         });
         
-        $('.top-recipes > ul > li').hover(function(){
+        $(document).on('mouseover', '.top-recipes > ul > li', function(){
            $(this).find('.star').removeClass('transDown').addClass('transUp');
            $(this).find('.upvotecnt').removeClass('transDown').addClass('transUp');
-        },function(){
+        });
+        $(document).on('mouseout', '.top-recipes > ul > li', function(){
            $(this).find('.upvotecnt').removeClass('transUp').addClass('transDown');
            $(this).find('.star').removeClass('transUp').addClass('transDown');
         });
     }
+
+    UserService.getUserProfile({username: $scope.username}).then(function(data) {
+        if(data.success) {
+            $scope.userprofile = data.results;            
+        }                        
+    }, function(error) {
+                          
+    }).catch(function(e) {
+                          
+    }).finally(function() {
+    });
 });
