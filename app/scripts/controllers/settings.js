444684407652f1f504691d3ecb0ba7497925f220
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-24 12:13:06 -0400

    Completed design of settings page

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -9,4 +9,23 @@
  */
 angular.module('whatsCookingApp').controller('SettingsCtrl', function ($rootScope, $scope, $window, SettingService) {    
 
+    $('.food-group-dropdown, .modal-food-group-dropdown').dropdown({
+		useLabels: false
+    });	
+
+    $('.male-gender-btn').on('click', function() {
+        // $scope.signUpModalProfile.gender = 'male';
+        $(this).attr('data-selected', true);
+        $('.female-gender-btn').removeClass('female-gender-selected');
+        $(this).addClass('male-gender-selected');
+        $('.female-gender-btn').attr('data-selected', false);        
+    });
+    $('.female-gender-btn').on('click', function() {   
+            // $scope.signUpModalProfile.gender = 'female';
+            $(this).attr('data-selected', true);
+            $('.male-gender-btn').removeClass('male-gender-selected');
+            $(this).addClass('female-gender-selected');
+            $('.male-gender-btn').attr('data-selected', false);
+    
+    });    
 });
