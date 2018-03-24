d10277bce59360fa4868c4b5782f71f23f276eed
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-23 20:38:54 -0400

    Added user service.

---
+++ app/scripts/services/userservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name whatsCookingApp.UserService
+ * @description
+ * # UserService
+ * Service in the whatsCookingApp.
+ */
+angular.module('whatsCookingApp')
+  .service('UserService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
