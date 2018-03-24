b5737fdf3298df94ec132c7a36d210acfb8c3c17
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-23 23:25:01 -0400

    EOD commit.

--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -7,7 +7,15 @@
  * # UserService
  * Service in the whatsCookingApp.
  */
-angular.module('whatsCookingApp')
-  .service('UserService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+angular.module('whatsCookingApp').service('UserService', function ($rootScope, $http, ApiConfig) {
+    var token = $rootScope.userProfile.token;    
+    
+    this.getUserProfile = function(payload) {
+        return $http({
+            method: 'POST',
+            url: ApiConfig.API_URL + '/Profile/userprofile',            
+            data: payload,
+            headers: {'x-api-key': btoa(ApiConfig.API_KEY),'Authorization': token}	
+        });
+    }
+});
