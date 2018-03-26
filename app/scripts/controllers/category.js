1d0afbdc0e959865a5d1359663dbd79a37f6b667
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-26 14:52:18 -0400

    Done basic design for category page.

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -7,11 +7,24 @@
  * # CategoryCtrl
  * Controller of the whatsCookingApp
  */
-angular.module('whatsCookingApp')
-  .controller('CategoryCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('whatsCookingApp').controller('CategoryCtrl', function ($rootScope, $scope, RecipeService) {    
+    $scope.recipes = [];
+
+    function getRecipes() {    
+        $('.loader-bg').show();
+        RecipeService.getRecipes().then(function(data) {
+            if(data.success) {
+                $scope.recipes = data.results;                
+            }                        
+        }, function(error) {
+                              
+        }).catch(function(e) {
+                              
+        }).finally(function() {            
+            $('.loader-bg').hide();
+        });
+    }    
+
+    getRecipes();
+
+});
